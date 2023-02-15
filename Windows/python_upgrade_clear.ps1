5f2787082a078c2d358fbcc848716c63436faa4c
Kage <2138096+GitKageHub@users.noreply.github.com>
2023-02-15 01:47:22 -0700

    added sanity checks

--- Windows/python_upgrade_clear.ps1
+++ Windows/python_upgrade_clear.ps1
@@ -1,8 +1,26 @@
-# Get a list of installed Python packages using Chocolatey
-$pythonPackages = choco list python -lo | Select-String -Pattern '^python'
+# Check if the script is running in administrative mode
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "This script requires administrative privileges. Please run the script as an administrator and try again."
+    Exit
+}
 
-# Sort the packages by version number
-$pythonPackages = $pythonPackages | Sort-Object -Descending { [version]($_ -split ' ')[-1] }
+# Check if Chocolatey is installed, and install it if it is not
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey is not installed. Attempting to install Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# Get a list of installed Python packages using Chocolatey, if available
+if (Get-Command choco -ErrorAction SilentlyContinue) {
+    $pythonPackages = choco list python -lo | Select-String -Pattern '^python'
+    $pythonPackages = $pythonPackages | Sort-Object -Descending {[version]($_ -split ' ')[-1]}
+}
+else {
+    Write-Host "Chocolatey is not available. Attempting to detect installed Python versions manually..."
+    $pythonPath = Get-ItemProperty -Path 'HKLM:\Software\Python\PythonCore\*' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty '(default)'
+    $pythonPackages = Get-ChildItem -Path $pythonPath -Directory | Select-Object -ExpandProperty Name | Where-Object { $_ -like 'Python*' }
+}
 
 # Get the latest version of Python
 $latestPythonVersion = $pythonPackages[0] -split ' ')[-1]
@@ -11,10 +29,21 @@ $latestPythonVersion = $pythonPackages[0] -split ' ')[-1]
 foreach ($package in $pythonPackages) {
     $version = ($package -split ' ')[-1]
     if ($version -ne $latestPythonVersion) {
-        choco upgrade $package -y
-        choco uninstall $package -y
+        if (Get-Command choco -ErrorAction SilentlyContinue) {
+            choco upgrade $package -y
+            choco uninstall $package -y
+        }
+        else {
+            $uninstallPath = Join-Path -Path $pythonPath -ChildPath $package
+            $uninstallArguments = "/uninstall /quiet"
+            Start-Process -FilePath "$uninstallPath\unins000.exe" -ArgumentList $uninstallArguments -Wait
+            Remove-Item -Path $uninstallPath -Recurse -Force
+        }
     }
 }
+
+# Upgrade pip
+python -m pip install --upgrade pip
 
 # Prompt the user before deleting virtual environments
 $virtualEnvironments = Get-ChildItem $env:USERPROFILE\Envs -Directory | Select-Object -ExpandProperty FullName
@@ -22,9 +51,9 @@ if ($virtualEnvironments.Count -gt 0) {
     $confirm = Read-Host "Are you sure you want to delete $($virtualEnvironments.Count) virtual environments? (y/n)"
     if ($confirm -eq 'y') {
         Remove-Item $virtualEnvironments -Recurse -Force
-        Write-Host 'Virtual environments deleted.'
+        Write-Host "Virtual environments deleted."
     }
 }
 else {
-    Write-Host 'No virtual environments found.'
+    Write-Host "No virtual environments found."
 }
