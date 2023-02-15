3b142e8b9f66eb884eaec7c5d313def108081042
Kage <2138096+GitKageHub@users.noreply.github.com>
2023-02-15 01:47:39 -0700

    formatting

--- Windows/python_upgrade_clear.ps1
+++ Windows/python_upgrade_clear.ps1
@@ -1,23 +1,23 @@
 # Check if the script is running in administrative mode
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "This script requires administrative privileges. Please run the script as an administrator and try again."
+    Write-Host 'This script requires administrative privileges. Please run the script as an administrator and try again.'
     Exit
 }
 
 # Check if Chocolatey is installed, and install it if it is not
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Host "Chocolatey is not installed. Attempting to install Chocolatey..."
+    Write-Host 'Chocolatey is not installed. Attempting to install Chocolatey...'
     Set-ExecutionPolicy Bypass -Scope Process -Force
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 # Get a list of installed Python packages using Chocolatey, if available
 if (Get-Command choco -ErrorAction SilentlyContinue) {
     $pythonPackages = choco list python -lo | Select-String -Pattern '^python'
-    $pythonPackages = $pythonPackages | Sort-Object -Descending {[version]($_ -split ' ')[-1]}
+    $pythonPackages = $pythonPackages | Sort-Object -Descending { [version]($_ -split ' ')[-1] }
 }
 else {
-    Write-Host "Chocolatey is not available. Attempting to detect installed Python versions manually..."
+    Write-Host 'Chocolatey is not available. Attempting to detect installed Python versions manually...'
     $pythonPath = Get-ItemProperty -Path 'HKLM:\Software\Python\PythonCore\*' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty '(default)'
     $pythonPackages = Get-ChildItem -Path $pythonPath -Directory | Select-Object -ExpandProperty Name | Where-Object { $_ -like 'Python*' }
 }
@@ -35,7 +35,7 @@ foreach ($package in $pythonPackages) {
         }
         else {
             $uninstallPath = Join-Path -Path $pythonPath -ChildPath $package
-            $uninstallArguments = "/uninstall /quiet"
+            $uninstallArguments = '/uninstall /quiet'
             Start-Process -FilePath "$uninstallPath\unins000.exe" -ArgumentList $uninstallArguments -Wait
             Remove-Item -Path $uninstallPath -Recurse -Force
         }
@@ -51,9 +51,9 @@ if ($virtualEnvironments.Count -gt 0) {
     $confirm = Read-Host "Are you sure you want to delete $($virtualEnvironments.Count) virtual environments? (y/n)"
     if ($confirm -eq 'y') {
         Remove-Item $virtualEnvironments -Recurse -Force
-        Write-Host "Virtual environments deleted."
+        Write-Host 'Virtual environments deleted.'
     }
 }
 else {
-    Write-Host "No virtual environments found."
+    Write-Host 'No virtual environments found.'
 }
