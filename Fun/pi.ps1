90b8c44f666ae29b0f442246a53d148fd85e1446
Kage <2138096+GitKageHub@users.noreply.github.com>
2023-02-15 01:21:28 -0700

    Initial commit
    Populate!

---
+++ Fun/pi.ps1
@@ -0,0 +1,17 @@
+# Define the number of iterations for the formula
+$iterations = 1000000
+
+# Initialize the sum to 0
+$sum = 0
+
+# Loop through the iterations and calculate the sum
+for ($i = 0; $i -lt $iterations; $i++) {
+    $term = [Math]::Pow(-1, $i) / (2 * $i + 1)
+    $sum += $term
+}
+
+# Multiply the sum by 4 to get an approximation of pi
+$pi = $sum * 4
+
+# Display the result
+Write-Host "Pi is approximately $pi."
---
+++ Windows/7dayerroreval.ps1
@@ -0,0 +1,22 @@
+# Set the time range to search for logs
+$StartTime = (Get-Date).AddDays(-7)
+$EndTime = Get-Date
+
+# Search for error events in the Windows event logs
+$Events = Get-WinEvent -FilterHashtable @{LogName='Application','System';Level=2;StartTime=$StartTime;EndTime=$EndTime}
+
+# Group the events by source and count the occurrences
+$EventGroups = $Events | Group-Object -Property Source
+
+# Loop through each event source
+foreach ($Group in $EventGroups) {
+    # Count the number of events in this group
+    $EventCount = $Group.Count
+    
+    # If there are more than 10 events in this group, it may be considered an unusual problem
+    if ($EventCount -gt 10) {
+        Write-Host "The following events from source $($Group.Name) occurred $($EventCount) times in the past 7 days:"
+        # Display the 10 most recent events from this group
+        $Group.Group | Select-Object -First 10 | Format-List | Out-String | Write-Host
+    }
+}
