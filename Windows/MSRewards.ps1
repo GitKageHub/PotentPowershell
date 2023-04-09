8742cd95853248580f802297880c8a8af0376ab9
Kage <2138096+GitKageHub@users.noreply.github.com>
2023-04-08 23:42:39 -0700

    rollback

--- Windows/MSRewards.ps1
+++ Windows/MSRewards.ps1
@@ -2,29 +2,14 @@ $edgePath = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
 $url = 'https://greybax.github.io/MSEdgeSearchClicker'
 
 # Check if Edge exists at the specified path
-if (-not (Test-Path $edgePath)) {
-    Write-Error "Microsoft Edge not found at '$edgePath'"
-    Exit 1
-}
-
-# Launch Edge with the specified URL in desktop mode
-try {
-    Start-Process -FilePath $edgePath -ArgumentList $url -ErrorAction Stop
-    # Wait for 60 seconds and terminate Edge
-    Start-Sleep -Seconds 30
-    Stop-Process -Name msedge -ErrorAction Stop
-} catch {
-    Write-Error "Failed to launch Microsoft Edge: $_"
-    Exit 1
-}
-
-# Launch Edge with the specified URL in mobile emulation mode
-try {
-    Start-Process -FilePath $edgePath -ArgumentList "--emulate-mobile", $url -ErrorAction Stop
-    # Wait for 60 seconds and terminate Edge
-    Start-Sleep -Seconds 30
-    Stop-Process -Name msedge -ErrorAction Stop
-} catch {
-    Write-Error "Failed to launch Microsoft Edge: $_"
-    Exit 1
+if (Test-Path $edgePath) {
+    # Launch Edge with the specified URL
+    try {
+        Start-Process -FilePath $edgePath -ArgumentList $url -ErrorAction Stop
+        # Wait for 60 seconds and terminate Edge
+        Start-Sleep -Seconds 30
+        Stop-Process -Name 'msedge' -ErrorAction Stop
+    } catch {
+        Exit 1
+    }
 }
